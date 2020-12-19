6ae3f855cea2905ec5612ed2c0af2f7729eb7250
Daniel Ryu <rujjiryu99@gmail.com>
2020-12-19 06:10:37 -0800

    implement better pagination

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -27,12 +27,27 @@ const PageContainer = styled.div`
   }
 `;
 
+const FooterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
 const StyledH3 = styled.h3`
   margin: 0 0 5px 0;
 `;
 
 const StyledButton = styled.button`
   padding: 0.4em 0.75em;
+`;
+
+const PageButton = styled.button`
+${p =>
+    p.selected &&
+  `
+    color: white;
+    background-color: blue;
+  `};
 `;
 
 const PageRange = ({ pageNum, movies }) => {
@@ -45,23 +60,49 @@ const PageRange = ({ pageNum, movies }) => {
   }
 };
 
-const PageButtonGroup = ({ pageNum, movies, total, handlePrevPage, handleNextPage }) => {
-  const prevDisabled = pageNum === 1 || pageNum === -1;
+const PageNav = ({ pageNum, total, movies, selectPage }) => {
+  const pageRangeLowerBound = (pageNum * 10) - 9;
+  const pageRangeUpperBound = ((pageNum - 1) * 10) + movies.length;
 
-  const pageUpperBound = ((pageNum - 1) * 10) + movies.length;
-  const nextDisabled = pageNum === MAX_PAGE_COUNT
-    || pageUpperBound >= total
-    || pageNum === -1;
+  const lastPageNum = Math.ceil(total / 10);
+
+  const lowerBoundPageNum = pageNum > 2 ? pageNum - 2 : 1;
+  const upperBoundPageNum = pageNum + 2 > lastPageNum ? lastPageNum : pageNum + 2;
+
+  const buttonsLength = upperBoundPageNum - lowerBoundPageNum + 1;
+
+  // array with vals 0...buttonsLength
+  const buttonsLengthNumList = [...Array(buttonsLength).keys()];
+
+  const prevDisabled = pageNum === 1 || pageNum === -1;
+  const nextDisabled = pageNum === lastPageNum || pageNum === -1;
 
   return (
-    <div>
-      <button onClick={handlePrevPage} disabled={prevDisabled}>
+    <FooterContainer>
+      <span>
+        {`Showing ${pageRangeLowerBound} to ${pageRangeUpperBound} of ${total} titles`}
+      </span>
+      <div>
+        <button onClick={() => selectPage(1)} disabled={prevDisabled}>
         &lt;
-      </button>
-      <button onClick={handleNextPage} disabled={nextDisabled}>
+        </button>
+        {buttonsLengthNumList.map(n => {
+          const buttonNum = n + lowerBoundPageNum;
+          return (
+            <PageButton
+              key={n}
+              onClick={() => selectPage(buttonNum)}
+              selected={pageNum === buttonNum}
+            >
+              {buttonNum}
+            </PageButton>
+          );
+        })}
+        <button onClick={() => selectPage(lastPageNum)} disabled={nextDisabled}>
         &gt;
-      </button>
-    </div>
+        </button>
+      </div>
+    </FooterContainer>
   );
 };
 
@@ -120,27 +161,43 @@ const SearchResults = React.memo(({ debouncedSearchText, nominations, setNominat
     );
   };
 
+  const selectPage = (pageNum) => {
+    searchMovies(debouncedSearchText, pageNum);
+  };
+
   return (
     <SearchResultsContainer>
       <HeaderContainer>
         <StyledH3>
           {
             debouncedSearchText.length === 0
-              ? 'Search up a movie!'
-              : `${searchData.total} results for "${debouncedSearchText}"`
+              ? 'Search for a movie!'
+              : `Results for "${debouncedSearchText}"`
           }
         </StyledH3>
-        <PageContainer>
+        {/* <PageContainer>
           <PageRange pageNum={pageNum} movies={searchData.movies} />
-          <PageButtonGroup
+          <PageNav
             pageNum={pageNum}
             movies={searchData.movies}
             total={searchData.total}
             handlePrevPage={() => searchMovies(debouncedSearchText, pageNum - 1)}
             handleNextPage={() => searchMovies(debouncedSearchText, pageNum + 1)}
           />
-        </PageContainer>
+        </PageContainer> */}
       </HeaderContainer>
+      {
+        !searchData.error ?
+          <FooterContainer>
+            <PageNav
+              pageNum={pageNum}
+              total={searchData.total}
+              movies={searchData.movies}
+              selectPage={selectPage}
+            />
+          </FooterContainer>
+          : null
+      }
       <MovieList
         movies={searchData.movies}
         MovieButton={NominateButton}
